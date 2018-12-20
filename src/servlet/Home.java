a9cfb1522f3b2cdbc2b2a39a4b9ee7052c9f91ba
exonihilo <erwan.geneve@icloud.com>
2018-12-20 21:20:43 +0100

    Remplissage de la liste RoomList

--- src/servlet/Home.java
+++ src/servlet/Home.java
@@ -1,8 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,8 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import bean.Area;
-import bean.AreaList;
 import bean.Room;
 import model.Roomlist;
 
@@ -37,34 +33,21 @@ public class Home extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		var rooms = new Roomlist();
-		var areas = new AreaList();
-		String param = (String)request.getParameter("id");
+		String param = request.getParameter("id");
 		Room room;
-		Area area;
 		
 		if(param == null) {
 			room = rooms.getRoom(0);
-			area = areas.getArea(0);
 			
 		} else {
 			room = rooms.getRoom(Integer.valueOf(param));
-			area = areas.getArea(Integer.valueOf(param));
 		}
 		
-		request.setAttribute("area", area);
 		request.setAttribute("room", room);
 		
 	
 		this.getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
 
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
